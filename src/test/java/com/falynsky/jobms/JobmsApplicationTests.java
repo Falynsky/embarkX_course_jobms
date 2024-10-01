568bc28bd53c9d69967ed577b819b946ddd55467
Falynsky <kamil.falinski@gmail.com>
2024-10-02 00:05:54 +0200

    git init

---
+++ src/test/java/com/falynsky/jobms/JobmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.falynsky.jobms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
